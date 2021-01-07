305bf1603c4d7d3e10847c4c98f1ddfb35f0392a
rwbfd <ran.wang.math@gmail.com>
2021-01-07 22:40:52 +0800

    Added steps per executation

--- torch_xla/distributed/parallel_loader.py
+++ torch_xla/distributed/parallel_loader.py
@@ -20,9 +20,11 @@ class PerDeviceQueue(object):
 
 class PerDeviceLoader(object):
 
-  def __init__(self, loader, device):
+  def __init__(self, loader, device, experimental_steps_per_execution=1):
     self._loader = loader
     self._device = device
+    self._steps_count = 0
+    self._experiment_steps_per_execution = experimental_steps_per_execution
 
   def __iter__(self):
     return self
@@ -34,7 +36,11 @@ class PerDeviceLoader(object):
     return self._loader.per_device_samples()
 
   def next(self):
-    xm.mark_step()
+    if self._steps_count % self._experiment_steps_per_execution == 0:
+      xm.mark_step()
+      self._steps_count = 0
+    else:
+      self._steps_count += 1
     item = self._loader.next_item(self._device)
     if item is None:
       raise StopIteration
@@ -52,6 +58,10 @@ class ParallelLoader(object):
       % len(devices)]`.
     batchdim (int, optional): The dimension which is holding the batch size.
       Default: 0
+    fixed_batch_size (bool, optional): Ensures that all the batch sizes sent to
+      the devices are of the same size. The original `loader` iteration stops as
+      soon as a not matching batch size is found.
+      Default: False
     loader_prefetch_size (int, optional): The max capacity of the queue used by
       the thread which is reading samples from the `loader`, to be processed by
       the worker threads which upload data to the devices.
@@ -60,23 +70,28 @@ class ParallelLoader(object):
       where the worker threads deposit tensors which have already been sent to
       devices.
       Default: 4
+    experimental_steps_per_execution (int, optional): How many steps to execute before calling
+    xm.mark_step(). This has a similar functionality as in tensorflow.
   """
 
   def __init__(self,
                loader,
                devices,
                batchdim=0,
+               fixed_batch_size=False,
                loader_prefetch_size=8,
-               device_prefetch_size=4):
+               device_prefetch_size=4,
+               experimental_steps_per_execution = 1):
     self._loader = loader
     self._devices = [torch.device(x) for x in devices]
     self._batchdim = batchdim
+    self._fixed_batch_size = fixed_batch_size
     self._per_device_samples = len(loader) // len(devices)
     self._done = False
     self._queues = dict()
     for device in self._devices:
       self._queues[device] = PerDeviceQueue(device, loader_prefetch_size,
-                                            device_prefetch_size)
+                                            device_prefetch_size, experimental_steps_per_execution)
     thread = threading.Thread(target=self._loader_worker)
     thread.daemon = True
     thread.start()
@@ -112,15 +127,34 @@ class ParallelLoader(object):
       dqueue.queue.close()
       dqueue.loader_queue.close()
 
+  def _get_batch_size(self, data, dim):
+    size = []
+
+    def fn(v):
+      csize = v.size()[dim]
+      if not size:
+        size.append(csize)
+      else:
+        assert csize == size[0]
+
+    xu.for_each_instance(data, lambda x: type(x) == torch.Tensor, fn)
+    return size[0] if size else None
+
   def _loader_worker(self):
     queues = list(self._queues.values())
     data_iter = enumerate(self._loader)
+    batch_size = None
     batch = []
     while not self._done:
       try:
         _, data = next(data_iter)
       except StopIteration:
         break
+      if self._fixed_batch_size:
+        if batch_size is None:
+          batch_size = self._get_batch_size(data, self._batchdim)
+        elif batch_size != self._get_batch_size(data, self._batchdim):
+          break
       batch.append(data)
       if len(batch) == len(self._devices):
         for queue_no, device_batch in enumerate(batch):
